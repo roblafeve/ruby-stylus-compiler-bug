e7a4c8b42564e14edd95ac76dd857f6ab3108871
Rob LaFeve <roblafeve@gmail.com>
2012-11-08 08:40:21 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StylusRails::Application.config.secret_token = 'e30281115235a26c631247df053fc8d39a17fc0a60cb74ad7221b7765dc636881f25352f6d5a790a210ddb9ece4f538198c272bd81570a1f8360512f6838802d'
